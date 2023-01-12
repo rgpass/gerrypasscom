5e1834c16714ad1e9d6df698943b10bfb86882f1
Gerry Pass <rgpass@gmail.com>
2023-01-11 23:02:08 -0500

    Fix warning and confirm working

--- src/@chakra-ui/gatsby-plugin/theme.js
+++ src/@chakra-ui/gatsby-plugin/theme.js
@@ -1,11 +1,12 @@
 // src/@chakra-ui/gatsby-plugin/theme.js
-import { extendTheme } from "@chakra-ui/react"
+import { baseTheme, extendTheme } from "@chakra-ui/react"
 const theme = {
   colors: {
     blue: {
-      500: "green",
+      500: "red",
     },
   },
 }
 
 export default extendTheme(theme)
+export { baseTheme }
