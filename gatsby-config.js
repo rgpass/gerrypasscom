88ff7777ae335a9edf892267d5b424652221805d
Gerry Pass <rgpass@gmail.com>
2023-01-11 22:48:55 -0500

    Add working example

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,6 +21,26 @@ module.exports = {
     },
   },
   plugins: [
+    {
+      resolve: "@chakra-ui/gatsby-plugin",
+      options: {
+        /**
+         * @property {boolean} [resetCSS=true]
+         * if false, this plugin will not use `<CSSReset />
+         */
+        resetCSS: true,
+        /**
+         * @property {boolean} [isUsingColorMode=true]
+         * if false, this plugin will not use <ColorModeProvider />
+         */
+        isUsingColorMode: true,
+        /**
+         * @property {boolean} [isBaseProvider=false]
+         * if true, will render `<ChakraBaseProvider>`
+         */
+        isBaseProvider: false,
+      },
+    },
     `gatsby-plugin-netlify`,
     `gatsby-plugin-image`,
     {
---
+++ src/@chakra-ui/gatsby-plugin/theme.js
@@ -0,0 +1,11 @@
+// src/@chakra-ui/gatsby-plugin/theme.js
+import { extendTheme } from "@chakra-ui/react"
+const theme = {
+  colors: {
+    blue: {
+      500: "green",
+    },
+  },
+}
+
+export default extendTheme(theme)
