2f5733e4864124e0146dd7e1d0284dec8c0e7793
Gerry Pass <rgpass@gmail.com>
2023-01-12 09:21:47 -0500

    Move to a HomePage dir

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,73 @@
+import { Link, PageProps } from "gatsby"
+import React, { FC } from "react"
+import Bio from "../../components/Bio"
+import Layout from "../../components/Layout/Layout"
+
+type Post = any
+
+type DataProps = {
+  allMdx: {
+    nodes: Post[]
+  }
+  site: {
+    siteMetadata?: {
+      title: string
+    }
+  }
+}
+
+export const HomePage: FC<PageProps<DataProps>> = ({ data, location }) => {
+  const siteTitle = data.site.siteMetadata?.title || `Title`
+  const posts = data.allMdx.nodes
+
+  if (posts.length === 0) {
+    return (
+      <Layout location={location} title={siteTitle}>
+        <Bio />
+        <p>
+          No blog posts found. Add markdown posts to "content/blog" (or the
+          directory you specified for the "gatsby-source-filesystem" plugin in
+          gatsby-config.js).
+        </p>
+      </Layout>
+    )
+  }
+
+  return (
+    <Layout location={location} title={siteTitle}>
+      <Bio />
+      <ol style={{ listStyle: `none` }}>
+        {posts.map(post => {
+          const title = post.frontmatter.title || post.fields.slug
+
+          return (
+            <li key={post.fields.slug}>
+              <article
+                className="post-list-item"
+                itemScope
+                itemType="http://schema.org/Article"
+              >
+                <header>
+                  <h2>
+                    <Link to={post.fields.slug} itemProp="url">
+                      <span itemProp="headline">{title}</span>
+                    </Link>
+                  </h2>
+                  <small>{post.frontmatter.date}</small>
+                </header>
+                <section>
+                  <p
+                    dangerouslySetInnerHTML={{
+                      __html: post.frontmatter.description || post.excerpt,
+                    }}
+                    itemProp="description"
+                  />
+                </section>
+              </article>
+            </li>
+          )
+        })}
+      </ol>
+    </Layout>
+  )
+}
