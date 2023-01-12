17fc6fd45adf90e58869a715fb7a4803d1c2a16c
Gerry Pass <rgpass@gmail.com>
2023-01-12 17:23:24 -0500

    Better align H1 with desired h1 size

--- src/components/mdx-components/H1.tsx
+++ src/components/mdx-components/H1.tsx
@@ -2,5 +2,5 @@ import React, { FC } from "react"
 import { Heading, HeadingProps } from "@chakra-ui/react"
 
 export const H1: FC<HeadingProps> = props => (
-  <Heading as="h1" size="3xl" {...props} />
+  <Heading as="h1" size="2xl" {...props} />
 )
