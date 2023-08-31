fa3ba49b75901fc31892723f94bcc057756d2316
Gerry Pass <rgpass@gmail.com>
2023-08-31 16:41:58 -0400

    Add Projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -3,47 +3,24 @@ import Image from 'next/image'
 
 import { Card } from '@/components/Card'
 import { SimpleLayout } from '@/components/SimpleLayout'
-import logoAnimaginary from '@/images/logos/animaginary.svg'
-import logoCosmos from '@/images/logos/cosmos.svg'
-import logoHelioStream from '@/images/logos/helio-stream.svg'
-import logoOpenShuttle from '@/images/logos/open-shuttle.svg'
-import logoPlanetaria from '@/images/logos/planetaria.svg'
+import logoPlz from '@/images/logos/plz.svg'
+import logoFlossSchedule from '@/images/logos/floss-schedule.png'
 
 const projects = [
   {
-    name: 'Planetaria',
-    description:
-      'Creating technology to empower civilians to explore space on their own terms.',
-    link: { href: 'http://planetaria.tech', label: 'planetaria.tech' },
-    logo: logoPlanetaria,
+    name: 'plz',
+    description: 'A CLI for converting natural language to shell commands',
+    link: { href: 'https://github.com/rgpass/plz', label: 'github.com' },
+    logo: logoPlz,
   },
   {
-    name: 'Animaginary',
-    description:
-      'High performance web animation library, hand-written in optimized WASM.',
-    link: { href: '#', label: 'github.com' },
-    logo: logoAnimaginary,
-  },
-  {
-    name: 'HelioStream',
-    description:
-      'Real-time video streaming library, optimized for interstellar transmission.',
-    link: { href: '#', label: 'github.com' },
-    logo: logoHelioStream,
-  },
-  {
-    name: 'cosmOS',
-    description:
-      'The operating system that powers our Planetaria space shuttles.',
-    link: { href: '#', label: 'github.com' },
-    logo: logoCosmos,
-  },
-  {
-    name: 'OpenShuttle',
-    description:
-      'The schematics for the first rocket I designed that successfully made it to orbit.',
-    link: { href: '#', label: 'github.com' },
-    logo: logoOpenShuttle,
+    name: 'Floss Schedule',
+    description: 'An IoT project for tracking my flossing habit',
+    link: {
+      href: 'https://github.com/rgpass/floss-schedule',
+      label: 'github.com',
+    },
+    logo: logoFlossSchedule,
   },
 ]
 
