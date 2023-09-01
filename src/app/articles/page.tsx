3bfbec852c98d6e39999744e0f8eba4be9c96bbc
Gerry Pass <rgpass@gmail.com>
2023-09-01 13:26:25 -0400

    Add Articles page

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -34,10 +34,12 @@ function Article({ article }: { article: ArticleWithSlug }) {
   )
 }
 
+const description =
+  'All of my long-form thoughts on software development, design, startups, and more, collected in chronological order.'
+
 export const metadata: Metadata = {
   title: 'Articles',
-  description:
-    'All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order.',
+  description,
 }
 
 export default async function ArticlesIndex() {
@@ -45,8 +47,8 @@ export default async function ArticlesIndex() {
 
   return (
     <SimpleLayout
-      title="Writing on software design, company building, and the aerospace industry."
-      intro="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
+      title="Writing on software development, design, and startups."
+      intro={description}
     >
       <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
         <div className="flex max-w-3xl flex-col space-y-16">
